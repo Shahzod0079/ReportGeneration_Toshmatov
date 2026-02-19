21e290b68b7c4e47ce719f938578d56fbb214318
Shahzod Toshmatov <toshmatovshahzod69@gmail.com>
2026-02-19 10:04:45 +0500

    Программирование пользовательского элемента Student. Тошматов

--- WpfApp1/WpfApp1/Items/Student.xaml.cs
+++ WpfApp1/WpfApp1/Items/Student.xaml.cs
@@ -1,28 +1,82 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using ReportGeneration_Toshmatov.Classes;
+using WpfApp1.Pages;
 
 namespace ReportGeneration_Toshmatov.Items
 {
-    /// <summary>
-    /// Логика взаимодействия для Student.xaml
-    /// </summary>
     public partial class Student : UserControl
     {
-        public Student()
+        private StudentContext student;
+        private Main mainPage;
+
+        public Student(StudentContext student, Main mainPage) 
         {
+            this.student = student;
+            this.mainPage = mainPage; 
             InitializeComponent();
+
+            TBFio.Text = $"{student.Lastname} {student.Firstname}";
+
+            CBExplored.IsChecked = student.Expelled;
+
+            // Используем mainPage вместо Main
+            List<DisciplineContext> StudentDisciplines = mainPage.AllDisciplines.FindAll(
+                x => x.IdGroup == student.IdGroup);
+
+            int NecessarilyCount = 0;
+            int WorksCount = 0;
+            int DoneCount = 0;
+            int MissedCount = 0;
+
+            foreach (DisciplineContext StudentDiscipline in StudentDisciplines)
+            {
+                List<WorkContext> StudentWorks = mainPage.AllWorks.FindAll(x =>
+                    (x.IdType == 1 || x.IdType == 2 || x.IdType == 3) &&
+                    x.IdDiscipline == StudentDiscipline.Id);
+
+                NecessarilyCount += StudentWorks.Count;
+
+                foreach (WorkContext StudentWork in StudentWorks)
+                {
+                    EvaluationContext Evaluation = mainPage.AllEvaluation.Find(x =>
+                        x.IdWork == StudentWork.Id &&
+                        x.IdStudent == student.Id);
+
+                    if (Evaluation != null && !string.IsNullOrWhiteSpace(Evaluation.Value) && Evaluation.Value != "2")
+                        DoneCount++;
+                }
+            }
+
+            List<WorkContext> AllStudentWorks = mainPage.AllWorks.FindAll(x =>
+                x.IdType != 4 && x.IdType != 3);
+
+            WorksCount += AllStudentWorks.Count;
+
+            foreach (WorkContext StudentWork in AllStudentWorks)
+            {
+                EvaluationContext Evaluation = mainPage.AllEvaluation.Find(x =>
+                    x.IdWork == StudentWork.Id &&
+                    x.IdStudent == student.Id);
+
+                if (Evaluation != null && !string.IsNullOrWhiteSpace(Evaluation.Lateness))
+                {
+                    if (int.TryParse(Evaluation.Lateness, out int lateness))
+                        MissedCount += lateness;
+                }
+            }
+
+            if (NecessarilyCount > 0)
+                doneWorks.Value = (100f / NecessarilyCount) * DoneCount;
+
+            if (WorksCount > 0)
+                missedCount.Value = (100f / (WorksCount * 90f)) * MissedCount;
+
+            TBGroup.Text = mainPage.Allgroups.Find(x => x.Id == student.IdGroup)?.Name ?? "";
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e) { }
     }
 }
